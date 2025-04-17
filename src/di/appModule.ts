5a4b527e8298a203a2b8323d59bfd116bc34aab9
Rohan <rohgyas@hotmail.com>
2025-04-18 01:24:22 +0800

    Fix typo

--- src/di/appModule.ts
+++ src/di/appModule.ts
@@ -1,14 +1,15 @@
 import apiClient from '../data/dataSources/remote/apiClient';
 import NewsApi from '../data/dataSources/remote/newsApi';
 import NewsRepositoryImpl from '../data/repository/newsRepositoryImpl';
-import GetMostViweedNewsUseCase from '../domain/useCases/getMostViewedNewsUseCase';
+import GetMostViewedNewsUseCase from '../domain/useCases/getMostViewedNewsUseCase';
+
 import getNewsFromSearchQueryUseCase from '../domain/useCases/getNewsFromSearchQueryUseCase';
 import GetTopStoriesByCatgoryUseCase from '../domain/useCases/getTopStoriesByCatgoryUseCase';
 
 class AppModule {
   newsApi: NewsApi;
   newsRepository: NewsRepositoryImpl;
-  getMostViewedNewsUseCase: GetMostViweedNewsUseCase;
+  getMostViewedNewsUseCase: GetMostViewedNewsUseCase;
   getTopStoriesByCategoryUseCase: GetTopStoriesByCatgoryUseCase;
   getNewsFromSearchQueryUseCase: getNewsFromSearchQueryUseCase;
 
@@ -20,7 +21,7 @@ class AppModule {
     this.newsRepository = new NewsRepositoryImpl(this.newsApi);
 
     // Initialize use cases
-    this.getMostViewedNewsUseCase = new GetMostViweedNewsUseCase(
+    this.getMostViewedNewsUseCase = new GetMostViewedNewsUseCase(
       this.newsRepository,
     );
     this.getTopStoriesByCategoryUseCase = new GetTopStoriesByCatgoryUseCase(
